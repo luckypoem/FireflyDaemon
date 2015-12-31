ad8d4c869ba4cbb2580e874ea80df5aa12040593
cbwfree <cbwfree@163.com>
2016-01-01 00:13:11 +0800

    fix

--- app/test.py
+++ app/test.py
@@ -4,6 +4,6 @@ import datetime
 
 def write_log(name):
     f = file("logs/%s.log" % name, "a+")
-    f.write(str(datetime.datetime.now()))
+    f.write(str(datetime.datetime.now())+"\n")
     f.flush()
     f.close()
